b8fa1e4f6ba8e08e9129431c1a01bee25061b59a
Heungsub Lee <sub@subl.ee>
2018-08-21 01:14:10 +0900

    We didn't need width range

--- width.go
+++ width.go
@@ -4,41 +4,40 @@ import (
 	"regexp/syntax"
 )
 
-// calcWidthRange calculates the range of the width for a Regexp pattern.
+// calcMaxWidth calculates the range of the width for a Regexp pattern.
 //
 // The range (min, max) is inclusive. -1 at the max means that unlimited.
 //
-func calcWidthRange(expr string) [2]int {
+func calcMaxWidth(expr string) int {
 	re, err := syntax.Parse(expr, syntax.Perl)
 
 	if err != nil {
 		// Failed to parse Regexp.
-		return [2]int{0, 0}
+		return 0
 	}
 
-	return _regexpWidthRange(re)
+	return _regexpMaxWidth(re)
 }
 
-// _regexpWidthRange calculates the range of the width for a parsed Regexp
+// _regexpMaxWidth calculates the range of the width for a parsed Regexp
 // pattern. It is used for calcWidthRange internally.
-func _regexpWidthRange(re *syntax.Regexp) [2]int {
+func _regexpMaxWidth(re *syntax.Regexp) int {
 	switch re.Op {
 
 	case syntax.OpNoMatch, syntax.OpEmptyMatch:
 		// matches no strings
 		// matches empty string
-		return [2]int{0, 0}
+		return 0
 
 	case syntax.OpLiteral:
 		// matches Runes sequence
-		n := len(re.Rune)
-		return [2]int{n, n}
+		return len(re.Rune)
 
 	case syntax.OpCharClass, syntax.OpAnyCharNotNL, syntax.OpAnyChar:
 		// matches Runes interpreted as range pair list
 		// matches any character except newline
 		// matches any character
-		return [2]int{1, 1}
+		return 1
 
 	case syntax.OpBeginLine, syntax.OpEndLine:
 		// matches empty string at beginning of line
@@ -51,75 +50,63 @@ func _regexpWidthRange(re *syntax.Regexp) [2]int {
 	case syntax.OpWordBoundary, syntax.OpNoWordBoundary:
 		// matches word boundary `\b`
 		// matches word non-boundary `\B`
-		return [2]int{0, 0}
+		return 0
 
 	case syntax.OpCapture:
 		// capturing subexpression with index Cap, optional name Name
-		return _regexpWidthRange(re.Sub0[0])
+		return _regexpMaxWidth(re.Sub0[0])
 
-	case syntax.OpStar:
+	case syntax.OpStar, syntax.OpPlus:
 		// matches Sub[0] zero or more times
-		return [2]int{0, -1}
-
-	case syntax.OpPlus:
 		// matches Sub[0] one or more times return -1
-		return [2]int{1, -1}
+		return -1
 
 	case syntax.OpQuest:
 		// matches Sub[0] zero or one times
-		nn := _regexpWidthRange(re.Sub0[0])
-		return [2]int{0, nn[1]}
+		return _regexpMaxWidth(re.Sub0[0])
 
 	case syntax.OpRepeat:
 		// matches Sub[0] at least Min times, at most Max (Max == -1 is no limit)
-		nn := _regexpWidthRange(re.Sub0[0])
-
-		min := re.Min * nn[0]
-		max := -1
-		if re.Max != -1 {
-			max = re.Max * nn[1]
+		if re.Max == -1 {
+			return -1
 		}
-
-		return [2]int{min, max}
+		return re.Max * _regexpMaxWidth(re.Sub0[0])
 
 	case syntax.OpConcat:
 		// matches concatenation of Subs
-		var min, max int
+		var total int
 
 		for _, sub := range re.Sub {
-			nn := _regexpWidthRange(sub)
+			n := _regexpMaxWidth(sub)
 
-			min += nn[0]
-
-			if nn[1] == -1 {
-				max = -1
-			} else if max != -1 {
-				max += nn[1]
+			if n == -1 {
+				return -1
 			}
+
+			total += n
 		}
 
-		return [2]int{min, max}
+		return total
 
 	case syntax.OpAlternate:
 		// matches alternation of Subs
-		min := -1
-		max := 0
+		var max int
 
 		for _, sub := range re.Sub {
-			nn := _regexpWidthRange(sub)
+			n := _regexpMaxWidth(sub)
 
-			if nn[0] < min || min == -1 {
-				min = nn[0]
+			if n == -1 {
+				return -1
 			}
 
-			if nn[1] > max && max != -1 || nn[1] == -1 {
-				max = nn[1]
+			if n > max {
+				max = n
 			}
 		}
 
-		return [2]int{min, max}
+		return max
 
 	default:
-		return [2]int{0, 0}
+		return 0
 	}
 }
